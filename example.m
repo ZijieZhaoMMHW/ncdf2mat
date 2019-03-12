593298ed80e431c5f62a9020653e7f69ae7e0f27
Zijie Zhao <44852627+ZijieZhaoMMHW@users.noreply.github.com>
2019-03-12 15:13:07 +1100

    Update example.m

--- example.m
+++ example.m
@@ -2,6 +2,6 @@
 % mat file in MATLAB. The name of mat file would be consistent with the
 % variable name in netcdf file.
 
-ncdf2mat('u850_hovo_2005_2017.nc');
+ncdf2mat('sst_get_land.nc');
 
 % Now the data in netcdf has been stored in mat file in your path
